b60afef6881d3813d4bd2be5d60799e7deb889e9
aniely.osilva <anielysilva08@gmail.com>
2019-08-01 17:18:15 -0300

    Ativ 01/08

--- app/src/main/java/com/example/atividadeum/DroidTwo.java
+++ app/src/main/java/com/example/atividadeum/DroidTwo.java
@@ -6,16 +6,16 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
-public class Droid_Activity extends AppCompatActivity {
+public class DroidTwo extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.droid_layout);
+        setContentView(R.layout.droid_two_layout);
     }
 
     public void abrirJanela(View view) {
-        Intent intent = new Intent(getApplicationContext(), DroidTwo.class);
+        Intent intent = new Intent(getApplicationContext(), Droid_Activity.class);
         startActivity(intent);
     }
 }
