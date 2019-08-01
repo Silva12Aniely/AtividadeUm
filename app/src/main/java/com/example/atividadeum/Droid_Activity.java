8a6e0cfd8412d99dd4bd78097953808edd231444
aniely.osilva <anielysilva08@gmail.com>
2019-08-01 14:53:40 -0300

    Initial commit

---
+++ app/src/main/java/com/example/atividadeum/Droid_Activity.java
@@ -0,0 +1,14 @@
+package com.example.atividadeum;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Droid_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.droid_layout);
+    }
+}
