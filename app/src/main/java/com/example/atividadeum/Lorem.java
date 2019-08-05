7d2f664756b8fbb038513b0d390715996711c37c
aniely.osilva <anielysilva08@gmail.com>
2019-08-05 15:12:52 -0300

    Ativ 05/08 - Up

---
+++ app/src/main/java/com/example/atividadeum/Lorem.java
@@ -0,0 +1,14 @@
+package com.example.atividadeum;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Lorem extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.layout_lorem);
+    }
+}
