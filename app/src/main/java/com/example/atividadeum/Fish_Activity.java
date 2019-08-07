d8ac45f30c60078504272755b9fad5556afc3242
aniely.osilva <anielysilva08@gmail.com>
2019-08-07 16:38:37 -0300

    07/08 Ativ up

---
+++ app/src/main/java/com/example/atividadeum/Fish_Activity.java
@@ -0,0 +1,14 @@
+package com.example.atividadeum;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Fish_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.fish_layout);
+    }
+}
--- app/src/main/java/com/example/atividadeum/Signup_Activity.java
+++ app/src/main/java/com/example/atividadeum/Signup_Activity.java
@@ -4,7 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class signup_Activity extends AppCompatActivity {
+public class Signup_Activity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
